7b22eb3cd4243d2faa9551fc8253162ba6fbc881
renattussupbayev-coder <renat.tussupbayev@gmail.com>
2026-04-23 13:52:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 import streamlit as st
+import requests
 
 st.title("Smart Irrigation MVP")
 
 lat = st.number_input("Latitude", value=43.2389)
 lon = st.number_input("Longitude", value=76.8897)
 
-st.write("Selected coordinates:")
-st.write("Latitude:", lat)
-st.write("Longitude:", lon)
+
+def get_weather(lat, lon):
+    url = (
+        "https://api.open-meteo.com/v1/forecast"
+        f"?latitude={lat}"
+        f"&longitude={lon}"
+        "&hourly=precipitation"
+        "&past_days=2"
+        "&forecast_days=2"
+        "&timezone=auto"
+    )
+
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+if st.button("Load Weather Data"):
+    try:
+        data = get_weather(lat, lon)
+
+        st.success("Weather data loaded successfully")
+        st.write(data)
+
+    except Exception as e:
+        st.error(f"Error: {e}")
