1ecae533641f87dabc0aa05631c31dbec4cdff84
renattussupbayev-coder <renat.tussupbayev@gmail.com>
2026-04-23 18:33:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-import streamlit as st
-
-st.title("Smart Irrigation DEBUG TEST")
-
-# 👉 Проверка запуска скрипта
-st.write("DEBUG: script is running")
-
-lat = st.number_input("Latitude", value=43.2389)
-lon = st.number_input("Longitude", value=76.8897)
-
-# 👉 Кнопка
-if st.button("Run AI Analysis"):
-    st.write("DEBUG: button clicked")
+st.title("FILE VERSION TEST 123")
