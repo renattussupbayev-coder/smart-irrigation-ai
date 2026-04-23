8b7136b76b480aee38ab70bfd7d70ca57e8549df
renattussupbayev-coder <renat.tussupbayev@gmail.com>
2026-04-23 13:51:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 st.title("Smart Irrigation MVP")
 
-st.write("Streamlit works")
+lat = st.number_input("Latitude", value=43.2389)
+lon = st.number_input("Longitude", value=76.8897)
+
+st.write("Selected coordinates:")
+st.write("Latitude:", lat)
+st.write("Longitude:", lon)
